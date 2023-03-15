c62ed68d6b9fd107a381faed70a3196b09d5bdf4
Leszek Lewandowski <leszek.lewandowski@dnv.com>
2023-03-15 13:47:13 +0100

    Initial

---
+++ ComarchZadania/ComarchZadania/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace ComarchZadania
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("KALCULATOR v1.0");
+
+            Console.WriteLine("Podaj opcję menu:");
+            Console.WriteLine(" 1. Dodawanie");
+
+            Console.Write("Podaj x: ");
+            string liczbaXstring = Console.ReadLine();
+
+            Console.Write("Podaj y: ");
+            string liczbaYstring = Console.ReadLine();
+
+            string sumaString = liczbaXstring + liczbaYstring;
+
+            int liczbaX = 0;
+            int liczbaY = 0;
+
+            if (int.TryParse(liczbaXstring, out liczbaX) 
+                && int.TryParse(liczbaYstring, out liczbaY))
+            {
+                int wynik = liczbaX + liczbaY;
+                Console.WriteLine($"Suma liczb {liczbaXstring} oraz {liczbaYstring} to {wynik}");
+            }
+            else
+            {
+                Console.WriteLine("Błędna liczba");
+            }
+            Console.ReadLine();
+        }
+    }
+}
